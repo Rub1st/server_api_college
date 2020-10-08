198c119af0c082cd1baba830b309389e0f6891e8
TakPohui <takpohuieslichestno@mail.ru>
2020-10-08 15:59:02 +0300

    Fix dependent destroy

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,6 @@
 class Organization < ApplicationRecord
   belongs_to :ownership_form
   has_many :warehouses, dependent: :destroy
-  has_many :contracts, foreign_key: :provider_id, dependent: :destroy
-  # has_many :contracts, foreign_key: :customer_id, dependent: :destroy
+  has_many :provider_contracts, class_name: 'Contract', foreign_key: :provider_id, dependent: :destroy
+  has_many :customer_contracts, class_name: 'Contract', foreign_key: :customer_id, dependent: :destroy
 end
--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,6 +1,10 @@
 class Warehouse < ApplicationRecord
   belongs_to :organization
   validates :address, :organization, presence: true
-  has_many :act_of_discrepancies, foreign_key: :provider_warehouse_id, dependent: :destroy
-  # has_many :act_of_discrepancies, foreign_key: :customer_warehouse_id, dependent: :destroy
+  has_many :provider_act_of_discrepancies, class_name: 'ActOfDiscrepancy',
+                                           foreign_key: :provider_warehouse_id,
+                                           dependent: :destroy
+  has_many :customer_act_of_discrepancies, class_name: 'ActOfDiscrepancy',
+                                           foreign_key: :customer_warehouse_id,
+                                           dependent: :destroy
 end
