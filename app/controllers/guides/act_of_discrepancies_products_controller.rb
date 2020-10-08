eb7387ec5520ab79e78338ee6292b421bf70ce4e
TakPohui <takpohuieslichestno@mail.ru>
2020-10-08 15:58:25 +0300

    Create invoice_product_parser method

--- app/controllers/guides/act_of_discrepancies_products_controller.rb
+++ app/controllers/guides/act_of_discrepancies_products_controller.rb
@@ -27,8 +27,24 @@ module Guides
 
     private
 
+    def invoice_product_parser(params)
+      {
+        short_name: params[:short_name],
+        full_name: params[:full_name],
+        code: params[:code],
+        price: params[:price],
+        rate_vat: RateVat.find(params[:rate_vat_id]),
+        summa_nds: params[:summa_nds],
+        cost: params[:cost],
+        unit: Unit.find(params[:unit_id]),
+        p_subgroup: PSubgroup.find(params[:p_subgroup_id]),
+        invoice: Invoice.find(params[:invoice_id])
+      }
+    end
+
     def post_method_helper (params)
       {
+        code: params[:code],
         invoice_product: InvoiceProduct.find(params[:invoice_product_id]),
         act_of_discrepancy: ActOfDiscrepancy.find(params[:act_of_discrepancy_id])
       }
@@ -36,14 +52,15 @@ module Guides
 
     def get_method_helper (el)
       {
-        invoice_product: el.invoice_product,
+        code: el.code,
+        invoice_product: invoice_product_parser(el.invoice_product),
         act_of_discrepancy: el.act_of_discrepancy
       }
     end
 
     def permit_params
       params.require(@model.name.underscore.to_sym)
-      .permit(:invoice_product_id, :act_of_discrepancy_id)
+      .permit(:invoice_product_id, :act_of_discrepancy_id, :code)
     end
   end
 end
